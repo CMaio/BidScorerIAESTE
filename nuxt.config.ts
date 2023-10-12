dbcf4412db30ccb60eab199b0ba2926bca0e5abd
Claudio Maiorana <50696746+CMaio@users.noreply.github.com>
2023-10-12 20:47:21 +0200

    Electron and tailwind added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-electron"],
+  electron: {
+    build: [
+      {
+        entry: "electron/main.ts",
+      },
+    ],
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
